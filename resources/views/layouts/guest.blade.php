f8228f358721e29b659cfe99dd6677c0fc94df3c
riyantoabuwinner <riyantoabuwinner@example.com>
2026-04-24 20:14:16 +0700

    Branding: Update APP_NAME to Risk Management and clean up layout titles

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Risk Management') }}</title>
 
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
