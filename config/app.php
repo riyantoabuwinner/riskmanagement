79744aa23e6d953bff29c8c3eb9b16fa7dfbac04
riyantoabuwinner <riyantoabuwinner@example.com>
2026-04-28 14:14:49 +0700

    Update app name to RiskManagement in config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Risk Management'),
+    'name' => env('APP_NAME', 'RiskManagement'),
 
     /*
     |--------------------------------------------------------------------------
